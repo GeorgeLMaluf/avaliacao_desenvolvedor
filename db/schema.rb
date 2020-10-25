feabe4bf87ee5ab208767bacdf82bb76fd8709ab
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2020-10-25 16:33:35 -0300

    Creating customer model and database migration

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema.define(version: 2020_10_25_193002) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_customers_on_name", unique: true
+  end
+
+end
