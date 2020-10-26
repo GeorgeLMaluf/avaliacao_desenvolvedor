c55b3f7e1638f764fe722df6c7cc740cd09f04d7
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2020-10-25 21:22:46 -0300

    Index action and page created

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,5 +1,10 @@
 class SalesController < ApplicationController
-  
+  include ActionView::Helpers::NumberHelper
+
   def index
+    @sales = Sale.all
+    @total_sales = @sales.map { |data| data[:unit_price].to_f * data[:quatity].to_f }.inject(':-')
   end
+
+  
 end
